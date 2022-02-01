88c7d1742aa7241c888a8db4114aa40f937de7f1
Science Done Right <sciencedoneright5@gmail.com>
2022-02-01 22:12:21 +0530

    Made some minor changes

--- bot.py
+++ bot.py
@@ -1,18 +1,29 @@
+# Imports
 import os
 import lightbulb
 import database
-database.initialize()
+
 if os.name != "nt":
     import uvloop
-
+    
     uvloop.install()
+    # Can I have a clear explanation to this?
 
+# Bot setup 
+database.initialize()
 with open("token", "r") as f:
-    token = f.read()
+    TOKEN = f.read()
 
 bot = lightbulb.BotApp(
-    token=token, prefix="b!", default_enabled_guilds=927835307989159977
+    token = TOKEN,
+    prefix = "b!",
+    default_enabled_guilds = (927835307989159977)
 )
+
+# NOTE TO VG08:
+# Add the token to a .py file, this is useful since you can add more secrets later and
+# is especially more useful when you deal with secrets that are not of type: str
+
 for file in os.listdir("extensions"):
     if file.endswith("py"):
         ext = file[:-3]
@@ -23,10 +34,15 @@ for file in os.listdir("extensions"):
 # Error Handling
 @bot.listen(lightbulb.CommandErrorEvent)
 async def on_error(event: lightbulb.CommandErrorEvent):
+
     if isinstance(event.exception, lightbulb.CommandNotFound):
+        # Runs when a command does not exist.
         pass
+
     elif isinstance(event.exception, lightbulb.NotEnoughArguments):
+        # Runs when all required arguments are not specified correctly.
         await event.context.respond(f"A required arguement is missing, try b!help {event.context.command.name}")
+    
     else:
         raise event.exception
 
