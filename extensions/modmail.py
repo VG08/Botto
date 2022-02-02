991821681ce03ebddff5f0fc8ce7f9fb446696f7
VG08 <71305756+VG08@users.noreply.github.com>
2022-02-02 09:46:37 +0530

    transition from json to sqlite

--- extensions/modmail.py
+++ extensions/modmail.py
@@ -2,110 +2,114 @@ import lightbulb
 import hikari
 import json
 import datetime
-
-
+import aiosqlite
+from .. import config
 plugin = lightbulb.Plugin("modmail")
 
+async def get_mail_by_user_id(user_id):
+    db = await aiosqlite.connect("bot.db")
+    sql = """SELECT * FROM modmail WHERE user_id=?"""
+    cursor = await db.execute(sql, (user_id,))
+    row = await cursor.fetchone()
+    return row
+async def get_mail_by_channel_id(channel_id):
+    db = await aiosqlite.connect("bot.db")
+    sql = """SELECT * FROM modmail WHERE channel_id=?"""
+    cursor = await db.execute(sql, (channel_id,))
+    row = await cursor.fetchone()
+    return row
+
+async def create_mail(user_id, channel_id):
+    db = await aiosqlite.connect("bot.db")
+    sql = """INSERT INTO modmail(user_id, channel_id) VALUES(?, ?) """
+    await db.execute(sql, (user_id, channel_id))
+    await db.commit()
 
 
+async def delete_mail(channel_id):
+    db = await aiosqlite.connect("bot.db")
+    sql = """DELETE FROM modmail WHERE channel_id = ?"""
+    await db.execute(sql, (channel_id,))
+    await db.commit()
 
 @plugin.command
 @lightbulb.command("cmail", "Closes the modmail in the current channel")
 @lightbulb.implements(lightbulb.SlashCommand)
 async def Cmail(ctx: lightbulb.Context) -> None:
-    f = open("modmails.json", "r")
-    mails = json.load(f)
-    m = mails.copy()
-    mails_channels = m.items()
-    for key, value in mails_channels:
-        if value == f"{ctx.channel_id}":
-            print(key)
-            user = await ctx.bot.rest.fetch_user(key)
-            dm = await user.fetch_dm_channel()
-            em = hikari.Embed(
-                title="Staff has closed this modmail thread, if you still have any problem feel free to dm me",
-                color=hikari.Colour.from_rgb(255, 0, 20),
-                timestamp=datetime.datetime.now(tz=datetime.timezone.utc),
-            )
-            guild = ctx.get_guild()
-            em.set_footer(f"Staff", icon=guild.icon_url)
-            await dm.send(embed=em)
-            # await dm.send("Staff has closed this modmail thread, if you still have any problem feel free to dm me")
-            channel = await ctx.bot.rest.fetch_channel(value)
-            print(mails)
-            print(mails[f"{key}"])
-            mails.pop(f"{key}")
+    row = await get_mail_by_channel_id(ctx.channel_id)
+    if row:
+        await delete_mail(ctx.channel_id)
+        user = await ctx.bot.rest.fetch_user(row[0])
+        dm = await user.fetch_dm_channel()
+        await ctx.get_channel().delete()
+        em = hikari.Embed(
+            title="Staff has closed this modmail thread, if you still have any problem feel free to dm me",
+            color=hikari.Colour.from_rgb(255, 0, 20),
+            timestamp=datetime.datetime.now(tz=datetime.timezone.utc),
+        )
+        guild = ctx.get_guild()
+        em.set_footer(f"Staff", icon=guild.icon_url)
+        await dm.send(embed=em)
 
-            await channel.delete()
-            f = open("modmails.json", "w")
-            json.dump(mails, f)
-            f.close()
-            return
-    await ctx.respond("Not a modmail thread")
+    else:
+        await ctx.respond("This channel is not a modmail thread")
+
+    # f = open("modmails.json", "r")
+    # mails = json.load(f)
+    # m = mails.copy()
+    # mails_channels = m.items()
+    # for key, value in mails_channels:
+    #     if value == f"{ctx.channel_id}":
+    #         print(key)
+
+    #         # await dm.send("Staff has closed this modmail thread, if you still have any problem feel free to dm me")
+    #         channel = await ctx.bot.rest.fetch_channel(value)
+    #         print(mails)
+    #         print(mails[f"{key}"])
+    #         mails.pop(f"{key}")
+
+    #         await channel.delete()
+    #         f = open("modmails.json", "w")
+    #         json.dump(mails, f)
+    #         f.close()
+    #         return
+    # await ctx.respond("Not a modmail thread")
 
 
 @plugin.listener(hikari.GuildMessageCreateEvent)
 async def onMessage(event: hikari.GuildMessageCreateEvent):
-    hpl = ["help vg", "vg help", "pybosh help", "where is vg", "vg!!"]
-    for i in hpl:
-        if i == event.message.content:
-            await event.message.respond(
-                f"<@734305495770333314>, Quick, <@{event.author_id}> needs help!"
-            )
-    hpl = ["help pybash", "pybash help", "pybosh help", "where is pybash", "pybash!!"]
-    for i in hpl:
-        if i == event.message.content:
-            await event.message.respond(
-                f"<@626461325744275464>, Quick, <@{event.author_id}> needs help!"
-            )
-
     if event.is_bot or not event.content:
         return
-    f = open("modmails.json", "r")
-    mails = json.load(f)
-    mails_channels = mails.items()
-    for key, value in mails_channels:
-        if value == f"{event.channel_id}":
-            print(key)
-            user = await plugin.bot.rest.fetch_user(key)
-            dm = await user.fetch_dm_channel()
-            em = hikari.Embed(
-                title=event.message.content,
-                color=hikari.Colour.from_rgb(0, 255, 20),
-                timestamp=datetime.datetime.now(tz=datetime.timezone.utc),
-            )
-            guild = event.get_guild()
-            em.set_footer(f"Staff", icon=guild.icon_url)
-            await dm.send(embed=em)
-
+    row = await get_mail_by_channel_id(event.channel_id)
+    print(row)
+    if row:
+        user = await  plugin.bot.rest.fetch_user(row[0])
+        dm = await user.fetch_dm_channel()  
+        em = hikari.Embed(
+            title=event.message.content,
+            color=hikari.Colour.from_rgb(0, 255, 20),
+            timestamp=datetime.datetime.now(tz=datetime.timezone.utc),
+        )
+        guild = event.get_guild()
+        em.set_footer(f"Staff", icon=guild.icon_url)
+        await dm.send(embed=em)
 
 @plugin.listener(hikari.DMMessageCreateEvent)
 async def onDM(event: hikari.DMMessageCreateEvent):
     if event.is_bot or not event.content:
         return
+
+    row = await get_mail_by_user_id(event.author_id)
     guild = plugin.bot.cache.get_guild(927835307989159977)
-    f = open("modmails.json", "r+")
-    mails = json.load(f)
-    try:
-        print(mails)
-        print(mails[str(event.author_id)])
 
-        f.close()
-        channel = plugin.bot.cache.get_guild_channel(mails[str(event.author_id)])
-    except KeyError as e:
-        print(e)
+    if row:
+        channel = plugin.bot.cache.get_guild_channel(row[1])
+    else:
         channel = await guild.create_text_channel(
-            name=event.author_id,
-            category=927841708392189963,
-        )
-        mails[f"{event.author_id}"] = f"{channel.id}"
-        await event.message.respond(
-            "Thanks for dming us, our staff will get to you soon"
-        )
-        f = open("modmails.json", "w")
-        json.dump(mails, f)
-        f.close()
-
+             name=event.author_id,
+             category=927841708392189963,
+         )
+        await create_mail(event.author_id, channel.id) 
     em = hikari.Embed(
         title=event.content,
         color=hikari.Colour.from_rgb(0, 255, 20),
@@ -116,6 +120,7 @@ async def onDM(event: hikari.DMMessageCreateEvent):
         icon=event.author.avatar_url,
     )
     await channel.send(embed=em)
+
 
 
 
