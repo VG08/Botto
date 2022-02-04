1c7292f9fefa39dfe17423af730f55873a997cba
VG08 <71305756+VG08@users.noreply.github.com>
2022-02-04 18:54:30 +0530

    intitail transitino

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
 GUILD_ID = 927835307989159977
 MODMAIL_CATEGORY = 927841708392189963
+with open("token", "r") as f:
+    token = f.read()
+TOKEN = token
+PREFIX = "b!"
+LAVALINK_PASSWORD = "lava123"
